68bed364785f39a2de378b8fc1fdd4287a0e69b2
Ferdinand Enario <ftenario@yahoo.com>
2018-02-06 22:25:03 -0800

    Add elixir_nsq package

--- mix.exs
+++ mix.exs
@@ -19,10 +19,14 @@ defmodule Dashboard.Mixfile do
   def application do
     [
       mod: {Dashboard.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools],
     ]
   end
 
+  # defp applications do
+  #   [:logger, :elixir_nsq]
+  # end
+  
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_),     do: ["lib"]
@@ -32,13 +36,15 @@ defmodule Dashboard.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:elixir_nsq, "~> 1.0.4"},
       {:phoenix, "~> 1.3.0"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:react_phoenix, "~> 0.5.0"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:poison, "~> 3.1", override: true}
     ]
   end
 end
